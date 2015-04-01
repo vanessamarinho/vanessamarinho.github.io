53f3452ea82845c45656e307a62d27428cbd1555
Vanessa <vanessa@snapeda.com>
2015-03-31 22:08:50 -0300

    Adding translated messages

--- js/mypage.js
+++ js/mypage.js
@@ -1,4 +1,22 @@
 angular.module('myPageApp', [])
   .controller('languageController',['$scope', function($scope) {
-  $scope.username = 'World';
+  $scope.menu = {};
+  
+  $scope.menu.research = ['Pesquisa','Research'];
+  $scope.menu.people = ['Pessoas','People'];
+  $scope.menu.interesting = ['Links Interessantes','Interesting Links'];
+  
+  /*0 stands for portuguese*/
+  $scope.language = 0;
+  
+  $("#portuguese").click(function(e){
+  	$scope.language = 0;
+	$scope.$apply();
+  });
+  
+  $("#english").click(function(e){
+  	$scope.language = 1;
+	$scope.$apply();
+  });
+  
 }]);
