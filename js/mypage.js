1407999c5cd2dfd1a5b10919b8b2d2e7d4b4194c
Vanessa <vanessa@snapeda.com>
2015-04-03 22:36:37 -0300

    Adding fontawesome

--- js/mypage.js
+++ js/mypage.js
@@ -4,6 +4,10 @@ angular.module('myPageApp', [])
   
   $scope.menu.research = ['Pesquisa','Research'];
   $scope.menu.people = ['Pessoas','People'];
+  $scope.menu.people.vanessa = ['Estudante de mestrado na Universidade de São Paulo (USP). Vanessa é formada em Ciências da Computação pela USP e foi aluna visitante na Universidade de Toronto pelo programa Ciência sem Fronteiras.',
+  "Graduate student at University of São Paulo (USP). Vanessa was an undergraduate student of Computer Science at USP. She was an exchange student at University of Toronto through the Science without Borders' program"];
+  $scope.menu.people.diego = ['Professor da Universidade de São Paulo (USP). Diego é formado em Engenharia da Computação pela USP e Doutor também pela USP. Sua tese de doutorado recebeu o Prêmio Tese Destaque USP.',''];
+  
   $scope.menu.interesting = ['Links Interessantes','Interesting Links'];
   
   $scope.page = {}
