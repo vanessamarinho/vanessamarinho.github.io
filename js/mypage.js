b3535778edd2d536f241203d6d4b37fd7ba9e2c7
Vanessa <vanessa@snapeda.com>
2015-04-04 16:49:43 -0300

    Changing file formats

--- js/mypage.js
+++ js/mypage.js
@@ -1,12 +1,12 @@
-angular.module('myPageApp', [])
+ï»¿angular.module('myPageApp', [])
   .controller('languageController',['$scope', function($scope) {
   $scope.menu = {};
   
   $scope.menu.research = ['Pesquisa','Research'];
   $scope.menu.people = ['Pessoas','People'];
-  $scope.menu.people.vanessa = ['Estudante de mestrado na Universidade de São Paulo (USP). Vanessa é formada em Ciências da Computação pela USP e foi aluna visitante na Universidade de Toronto pelo programa Ciência sem Fronteiras.',
-  "Graduate student at University of São Paulo (USP). Vanessa did her undergraduate course in Computer Science at USP. She was an exchange student at University of Toronto through the Science without Borders' program"];
-  $scope.menu.people.diego = ['Professor da Universidade de São Paulo (USP). Diego é formado em Engenharia da Computação pela USP e Doutor também pela USP. Sua tese de doutorado recebeu o Prêmio Tese Destaque USP.',''];
+  $scope.menu.people.vanessa = ['Estudante de mestrado na Universidade de SÃ£o Paulo (USP). Vanessa Ã© formada em CiÃªncias da ComputaÃ§Ã£o pela USP e foi aluna visitante na Universidade de Toronto pelo programa CiÃªncia sem Fronteiras.',
+  "Graduate student at University of SÃ£o Paulo (USP). Vanessa did her undergraduate course in Computer Science at USP. She was an exchange student at University of Toronto through the Science without Borders' program"];
+  $scope.menu.people.diego = ['Professor da Universidade de SÃ£o Paulo (USP). Diego Ã© formado em Engenharia da ComputaÃ§Ã£o pela USP e Doutor tambÃ©m pela USP. Sua tese de doutorado recebeu o PrÃªmio Tese Destaque USP.',''];
   
   $scope.menu.interesting = ['Links Interessantes','Interesting Links'];
   
