84242181257abfbeb37b299e15f8b2140c17bbaa
Vanessa <vanessa@snapeda.com>
2015-04-12 00:18:39 -0300

    Adding Diego photo

--- js/mypage.js
+++ js/mypage.js
@@ -6,9 +6,9 @@
   
   $scope.menu.research = ['Pesquisa','Research'];
   $scope.menu.people = ['Pessoas','People'];
-  $scope.menu.people.vanessa = ['Estudante de mestrado na Universidade de São Paulo (USP). Vanessa é formada em Ciências da Computação pela USP e foi aluna visitante na Universidade de Toronto pelo programa Ciência sem Fronteiras.',
+  $scope.menu.people.vanessa = ['Estudante de mestrado na Universidade de São Paulo (USP). Vanessa é formada em Ciências da Computação pela USP e foi aluna visitante na Universidade de Toronto pelo programa Ciência sem Fronteiras por um ano.',
   "Graduate student at University of São Paulo (USP). Vanessa did her undergraduate course in Computer Science at USP. She was an exchange student at University of Toronto through the Science without Borders' program"];
-  $scope.menu.people.diego = ['Professor da Universidade de São Paulo (USP). Diego é formado em Engenharia da Computação pela USP e Doutor também pela USP. Sua tese de doutorado recebeu o Prêmio Tese Destaque USP.',
+  $scope.menu.people.diego = ['Professor Doutor da Universidade de São Paulo (USP). Diego é formado em Engenharia de Computação pela USP e Doutor também pela USP. Sua tese de doutorado recebeu o Prêmio Tese Destaque USP.',
   "Professor at University of São Paulo (USP). Diego did his undergraduate course in Computer Engineering at USP and he received his PhD at USP. His PhD thesis was selected as the best one at USP between 2012 and 2013."];
   
   $scope.menu.interesting = ['Links Interessantes','Interesting Links'];
